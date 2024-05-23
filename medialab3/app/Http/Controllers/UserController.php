51f2a810dd37bd2fa684eb4896f9d158670c43ff
Bbelkasem <163291966+BilalBelkasem@users.noreply.github.com>
2024-05-24 00:58:53 +0200

    Update UserController.php

--- medialab3/app/Http/Controllers/UserController.php
+++ medialab3/app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\user;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $users = user::all();
+        return view('user', compact('users'));
+    }
 }
